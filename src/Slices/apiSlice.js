f2bd3ed28832104b544782b9de366cc07bbd7fa6
NoobVenom <mirjiprajwal15@gmail.com>
2024-06-25 20:41:32 +0530

    .

--- src/Slices/apiSlice.js
+++ src/Slices/apiSlice.js
@@ -1,11 +1,7 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 
 const baseQuery = fetchBaseQuery({ baseUrl: '' }); // Set your backend server URL
-<<<<<<< HEAD
-// const baseQuery = fetchBaseQuery({ baseUrl: 'http://localhost:8080', credentials: 'include' }); // Set your backend server URL
-=======
 // const baseQuery = fetchBaseQuery({ baseUrl: 'https://inventory-backend-isolated.onrender.com' ,credentials:'include'}); // Set your backend server URL
->>>>>>> f666f572778d067c4f5f71f0ddb0df0d0f75f281
 
 export const apiSlice = createApi({
     baseQuery,
