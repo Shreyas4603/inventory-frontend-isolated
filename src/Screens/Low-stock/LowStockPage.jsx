7d9e12682014c9f24356efc507e3697b7670a066
rakshith-git <rakshith.dope@gmail.com>
2024-06-25 21:01:22 +0530

    added low stock page

---
+++ src/Screens/Low-stock/LowStockPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const LowStockPage = () => {
+  return (
+    <div className='  text-5xl font-jakarta'>
+      Low stocks
+    </div>
+  )
+}
--- src/Screens/index.js
+++ src/Screens/index.js
@@ -1,6 +1,7 @@
-export {Dashboard} from './Dashboard/Dashboard'
-export {Inventory} from './Inventory/Inventory'
-export {AddProduct} from './Inventory/AddProduct'
-export {ViewAndEditProduct} from './Inventory/ViewAndEditProduct'
+export { Dashboard } from './Dashboard/Dashboard'
+export { LowStockPage } from './Low-stock/LowStockPage'
+export { Inventory } from './Inventory/Inventory'
+export { AddProduct } from './Inventory/AddProduct'
+export { ViewAndEditProduct } from './Inventory/ViewAndEditProduct'
 
 //folldername change
